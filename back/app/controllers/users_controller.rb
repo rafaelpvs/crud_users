6ba9b42839d3a91a39a4c83da19cfb954d796b66
rafaelpvs <rafael12wq@gmail.com>
2025-06-10 15:30:42 -0300

    create user crud

---
+++ back/app/controllers/users_controller.rb
@@ -0,0 +1,37 @@
+class UsersController < ApplicationController
+	before_action :set_users, only: :index
+	def index; end
+
+  def create
+		@user = User.new(user_params)
+		if @user.save
+			render :show, status: :created
+		else
+			render json: {errors: @user.errors.full_messages}
+		end
+  end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			render :show, status: :ok
+		else
+			render json: {errors: @user.errors.full_messages}
+		end
+	end
+
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		render :destroy, status: :no_content
+	end
+
+	private
+	def user_params
+		params.require(:user).permit(:first_name, :last_name, :birth_date)
+	end
+
+	def set_users
+		@users = User.all
+	end
+end
--- back/config/routes.rb
+++ back/config/routes.rb
@@ -6,5 +6,8 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "users#index"
+  defaults format: :json do
+    resources :users, only: [:create, :index, :destroy, :update]
+  end
 end
