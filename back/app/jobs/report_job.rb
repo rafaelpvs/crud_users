031ec1660ab54fe939a5125405efe514fc3b45f2
Rafael <rafael12wq@gmail.com>
2025-06-27 00:34:34 -0300

    use ApplicationController.renderer.render

--- back/app/jobs/report_job.rb
+++ back/app/jobs/report_job.rb
@@ -1,16 +1,18 @@
 class ReportJob < ApplicationJob
   queue_as :default
 
+  # retry_on StandardError, wait: 5.seconds, attempts: 3
+
   def perform(template, query_params)
     filters = JSON.parse(query_params)
     users = User.ransack(filters).result
-    excel_data = render_to_string(
+    excel_data = ApplicationController.renderer.render(
       handlers: [ :axlsx ],
       formats: [ :xlsx ],
       template: template,
       layout: false,
-      assigns: { users: users }
+      locals: { users: users }
     )
-    UserMailer.export_email(excel_data).deliver_later
+    UserMailer.export_email(excel_data).deliver_now
   end
 end
