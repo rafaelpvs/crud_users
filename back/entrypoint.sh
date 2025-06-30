40b2485890dfc4a0cb9e2e95b3d3b1f00a7c7cdd
Rafael <rafael12wq@gmail.com>
2025-06-30 00:15:29 -0300

    dockerize also front application

--- back/entrypoint.sh
+++ back/entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 set -e
+rm -f tmp/pids/server.pid
+echo "Aguardando o banco de dados ficar pronto..."
 
-# echo "Aguardando o banco de dados ficar pronto..."
-
-# until pg_isready -h "$DATABASE_HOST" -p 5432 -U "$DATABASE_USERNAME"; do
-#   sleep 1
-# done
+until pg_isready -h "$DATABASE_HOST" -p 5432 -U "$DATABASE_USERNAME"; do
+  sleep 1
+done
 
 echo "Banco de dados pronto. Rodando migrations..."
 
--- front/src/services/AxiosInstance.ts
+++ front/src/services/AxiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: import.meta.env.VITE_FRONT_API_BASE_URL,
   timeout: 100000,
   headers: {
     "Content-Type": "application/json",
